bff31845201e293ef7a21e66782432fb5fbc7524
Aditi Sharma <aditi.sharma.7946@gmail.com>
2020-10-05 17:58:34 +0530

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
    get 'about'   => 'static_pages#about'
    get 'contact' => 'static_pages#contact'
    get 'signup'  => 'users#new'
+   resources :users  #routing for 1users/1
 #  root 'static_pages#home'
   #get 'static_pages/home'
 #  get 'static_pages/help'
