58f5a5f268883bee0438a5dd0a569b8eab591533
Aditi Sharma <aditi.sharma.7946@gmail.com>
2020-10-07 13:54:07 +0530

    log

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'sessions/new'
+  #get 'users/new'
   root             'static_pages#home'
    get 'help'    => 'static_pages#help'
    get 'about'   => 'static_pages#about'
    get 'contact' => 'static_pages#contact'
    get 'signup'  => 'users#new'
+
+   get 'login' =>'sessions#new'
+   get 'login' =>'sessions#create'
+   delete 'logout' =>'sessions#destroy'
+
+   resources :users
 #  root 'static_pages#home'
   #get 'static_pages/home'
 #  get 'static_pages/help'
